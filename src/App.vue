271233f7ea86c289cdcd5c94f28482b5341049a4
Alberto Rojas <shaka0241@gmail.com>
2024-11-12 00:15:10 +0100

    Add Horario component and replace HorarioSemanal component; update App.vue to include Horario in the template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script>
 import Alberto from './components/Alberto.vue'
 import Diego from './components/Diego.vue'
+import Horario from './components/Horario.vue'
 export default {
   name: 'App',
   components: {
     Alberto,
     Diego,
+    Horario
   }
 }
 </script>
 
 <template>
   <Diego />
+  <Horario />
 </template>
 
 <style scoped>
---
+++ src/components/Horario.vue
@@ -0,0 +1,49 @@
+<template>
+  <h2 class="text-2x1 font-bold mb-4 text-center text-green-700">
+    Horario Semanal
+  </h2>
+  <div class="container">
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Hora</th>
+      <th scope="col">Lunes</th>
+      <th scope="col">Martes</th>
+      <th scope="col">Miércoles</th>
+      <th scope="col">Jueves</th>
+      <th scope="col">Viernes</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Horario",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
