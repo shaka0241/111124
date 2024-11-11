cfb19dfc9bd4b2da210078e276a9a327ca068bc5
Alberto Rojas <shaka0241@gmail.com>
2024-11-11 22:45:36 +0100

    Replace HelloWorld component with Alberto and simplify App.vue layout; add Alberto.vue component with sample content

---
+++ src/components/Alberto.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1 class="display-4">Alberto</h1>
+    <p>Soy el cid Campeador de AOE2</p>
+    <ul class="list-group">
+        <li class="list-group-item">
+            Soy yo
+        </li>
+        <li class="list-group-item">
+            Muchos años
+        </li>
+        <li class="list-group-item">
+            Saludos a todos
+        </li>
+      </ul>
+</template>
+
+<script>
+export default {
+    name: 'Alberto',
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style scoped>
+</style>
